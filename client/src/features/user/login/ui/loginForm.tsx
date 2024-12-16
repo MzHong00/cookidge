41b3c666d0ea9f2648bf9b65b90b0ba95de7e40e
unknown <rkawk1539@gmail.com>
2024-12-16 16:03:42 +0900

    로그인 폼 framer fade layout 적용 및 로고에 Like 적용

--- client/src/features/user/login/ui/loginForm.tsx
+++ client/src/features/user/login/ui/loginForm.tsx
@@ -1,33 +1,44 @@
+import { Link } from "react-router-dom";
 import { FcGoogle } from "@react-icons/all-files/fc/FcGoogle";
 
 import { Logo } from "shared/ui/logo";
 import { IconBox } from "shared/ui/iconBox";
+import { OAuthService } from "shared/api/oauth";
 import { SubjectBox } from "shared/ui/subjectBox";
-import { OAuthService } from "../../../../shared/api/oauth/service";
+import { FramerFadeLayout } from "shared/ui/framerFadeLayout";
 
 import styles from "./loginForm.module.scss";
 
-interface Props extends React.HTMLAttributes<HTMLDivElement> {}
+interface Props {
+  className: string;
+}
 
-export const LoginForm = ({ className, ...props }: Props) => {
+export const LoginForm = ({ className }: Props) => {
   const googleOAuthHandler = async () => {
     await OAuthService.googleOAuth();
-  }
+  };
 
   return (
-    <SubjectBox className={`${styles.container} ${className}`} {...props}>
-      <Logo className={styles.logo} />
+    <FramerFadeLayout className={`${styles.container} ${className}`}>
+      <SubjectBox className={styles.loginForm}>
+        <Link to="/">
+          <Logo className={styles.logo} />
+        </Link>
 
-      <main className={styles.content}>
-        <div className={styles.introduce}>
-          <h3>시작하기</h3>
-          <p>계정에 로그인하여 다양한 서비스를 경험하세요.</p>
-        </div>
-        <button className={`${styles.oauthButton} ${styles.googleOAuth}`} onClick={googleOAuthHandler}>
-          <IconBox Icon={FcGoogle} className={styles.oauthIcon} />
-          <b>구글 계정으로 로그인</b>
-        </button>
-      </main>
-    </SubjectBox>
+        <main className={styles.content}>
+          <div className={styles.introduce}>
+            <h3>시작하기</h3>
+            <p>계정에 로그인하여 다양한 서비스를 경험하세요.</p>
+          </div>
+          <button
+            className={`${styles.oauthButton} ${styles.googleOAuth}`}
+            onClick={googleOAuthHandler}
+          >
+            <IconBox Icon={FcGoogle} className={styles.oauthIcon} />
+            <b>구글 계정으로 로그인</b>
+          </button>
+        </main>
+      </SubjectBox>
+    </FramerFadeLayout>
   );
 };
