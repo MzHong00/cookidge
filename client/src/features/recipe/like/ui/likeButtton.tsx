35a22e35e9b5c562f5204d0cac559d72fe5b06b0
unknown <rkawk1539@gmail.com>
2024-12-28 21:22:09 +0900

    레시피 폼 모든 속성을 채워야 검증이 통과하는 것으로 변경

--- client/src/features/recipe/like/ui/likeButtton.tsx
+++ client/src/features/recipe/like/ui/likeButtton.tsx
@@ -1,9 +1,8 @@
 import { useNavigate } from "react-router-dom";
-import { useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { RiHeart3Line } from "@react-icons/all-files/ri/RiHeart3Line";
 import { RiHeart3Fill } from "@react-icons/all-files/ri/RiHeart3Fill";
 
-import { IUser } from "shared/api/user";
 import { IRecipe } from "shared/api/recipe";
 import { IconButton } from "shared/ui/iconButton";
 import {
@@ -11,7 +10,6 @@ import {
   useUnlikeMutation,
 } from "../mutation/useLikeMutation";
 import { UserQueries } from "entities/user";
-import { useMemo } from "react";
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   recipe_id: IRecipe["_id"];
@@ -25,16 +23,12 @@ export const LikeButton = ({
   ...props
 }: Props) => {
   const navigate = useNavigate();
-  const queryClient = useQueryClient();
+  const { data: me } = useQuery(UserQueries.meQuery());
 
-  const me = queryClient.getQueryData<IUser | undefined>([UserQueries.keys.me]);
   const { mutate: likeMutate } = useLikeMutation(recipe_id);
   const { mutate: unlikeMutate } = useUnlikeMutation(recipe_id);
 
-  const isLike = useMemo(
-    () => (me?._id ? likeMembers.includes(me?._id) : false),
-    [likeMembers, me]
-  );
+  const isLike = me?._id && likeMembers.includes(me?._id);
 
   const onClickLike = () => {
     if (!me) return navigate("/login");
@@ -48,28 +42,17 @@ export const LikeButton = ({
     unlikeMutate();
   };
 
-  if (isLike)
-    return (
-      <IconButton
-        Icon={() => <RiHeart3Fill color="red" />}
-        onClick={onClickUnlike}
-        style={{
-          paddingInline: 0,
-          ...(disabled && { cursor: "default" }),
-        }}
-        disabled={disabled}
-        {...props}
-      >
-        {likeMembers.length}
-      </IconButton>
-    );
-
   return (
     <IconButton
-      Icon={() => <RiHeart3Line color="red" />}
-      onClick={onClickLike}
+      Icon={() =>
+        isLike ? <RiHeart3Fill color="red" /> : <RiHeart3Line color="red" />
+      }
+      onClick={isLike ? onClickUnlike : onClickLike}
+      style={{
+        paddingInline: 0.25,
+        ...(disabled && { pointerEvents: "none" }),
+      }}
       disabled={disabled}
-      style={{ ...(disabled && { cursor: "default" }) }}
       {...props}
     >
       {likeMembers.length}
