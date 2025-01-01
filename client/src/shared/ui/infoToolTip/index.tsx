712ece2464514ed7d684177ae20e6ca6168035e6
unknown <rkawk1539@gmail.com>
2025-01-01 20:21:54 +0900

    툴팁 컴포넌트 Div Element 확장 및 폰트 컬러 검정으로 변경

--- client/src/shared/ui/infoToolTip/index.tsx
+++ client/src/shared/ui/infoToolTip/index.tsx
@@ -2,13 +2,13 @@ import { RiInformationLine } from "@react-icons/all-files/ri/RiInformationLine";
 
 import styles from "./index.module.scss";
 
-interface Props {
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
   message: string;
 }
 
-export const InfoTooltip = ({ message }: Props) => {
+export const InfoTooltip = ({ message, className, ...props }: Props) => {
   return (
-    <div className={styles.tooltipWrapper}>
+    <div className={`${styles.tooltipWrapper} ${className}`} {...props}>
       <RiInformationLine className={styles.icon} />
       <span className={styles.tooltipText}>{message}</span>
     </div>
