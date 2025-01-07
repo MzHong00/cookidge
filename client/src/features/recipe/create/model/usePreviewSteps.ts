1d84f6e0a93d6e12158eea88914e0f90ae2b5d3b
unknown <rkawk1539@gmail.com>
2025-01-07 13:35:23 +0900

    preview 에러 해결 다시 시도

--- client/src/features/recipe/create/model/usePreviewSteps.ts
+++ client/src/features/recipe/create/model/usePreviewSteps.ts
@@ -4,30 +4,28 @@ import type { ICookingStepInput } from "shared/api/recipe";
 
 export const usePreviewSteps = (stepState: ICookingStepInput[]) => {
   const [previewImage, setPreviewImage] = useState<string[]>([]);
-
   // Effect 의존성 배열의 깊은 비교를 하기 위한 diffStep
   // JSON.stringify를 직접 넣으면 경고 메시지 발생하기 때문에 변수로 할당
-  const diffStep = JSON.stringify(stepState.map((step) => step.picture));
-
+  const diffStep =  JSON.stringify(stepState.map(({picture})=>typeof picture === "string" ? picture : picture?.[0]?.name))
+  
   useEffect(() => {
     const imageFiles = stepState.map((step) => step.picture);
+    
     const previewUrls = imageFiles.map((file) => {
-      if (!file) return "";
+      if (!file?.length) return "";
 
       return typeof file === "string" ? file : URL.createObjectURL(file[0]);
     });
 
     setPreviewImage(previewUrls);
-  }, [stepState, diffStep]);
 
-  useEffect(() => {
     return () => {
-      previewImage.forEach((url) => {
-        if (!url.startsWith("blob:")) return;
+      previewUrls.forEach((url) => {
+        if (!url) return;
         return URL.revokeObjectURL(url);
       });
     };
-  }, []);
+  }, [stepState, diffStep]);
 
   return previewImage;
 };
