76af66e034b41529bf23de9fbd1dfad54ebc8df5
unknown <rkawk1539@gmail.com>
2025-01-08 16:04:58 +0900

    레시피 폼 너무 길어서 field들을 컴포넌트로 나눔

--- client/src/features/recipe/create/ui/recipeForm/recipeForm.tsx
+++ client/src/features/recipe/create/ui/recipeForm/recipeForm.tsx
@@ -1,34 +1,29 @@
 import {
   useForm,
   SubmitHandler,
-  useFieldArray,
-  UseFormReturn,
   SubmitErrorHandler,
 } from "react-hook-form";
-import { RiAddLine } from "@react-icons/all-files/ri/RiAddLine";
-import { CgRemoveR } from "@react-icons/all-files/cg/CgRemoveR";
 import { RiGroupLine } from "@react-icons/all-files/ri/RiGroupLine";
 import { RiTimer2Line } from "@react-icons/all-files/ri/RiTimer2Line";
 
-import type { IRecipeForm } from "shared/api/recipe/type";
+import type { ICookingStep, IRecipeForm } from "shared/api/recipe/type";
 import { InputBox } from "shared/ui/inputBox";
+import { TextArea } from "shared/ui/textArea";
 import { InputFile } from "shared/ui/inputFile";
 import { SubjectBox } from "shared/ui/subjectBox";
-import { IconButton } from "shared/ui/iconButton";
 import { FadeLayout } from "shared/ui/fadeLayout";
 import { InfoTooltip } from "shared/ui/infoToolTip";
 import { useConfirmDialogActions } from "shared/ui/confirmDialog";
 import { CldImg } from "shared/ui/cloudinaryImage/cloudinaryImage";
 import { usePreviewImages } from "shared/hooks/usePreviewImages";
 import { FOOD_CATEGORIES } from "entities/recipe";
-import { INGREDIENTS_CATEGORIES } from "entities/ingredient";
-
-import { usePreviewSteps } from "../..";
+import { StepField } from "./StepField";
+import { IngredientField } from "./ingredientField";
 
 import styles from "./recipeForm.module.scss";
 
 const NAME_LIMIT_LENGTH = 20;
-const INTRODUCE_LIMIT_LENGTH = 200;
+const INTRODUCE_LIMIT_LENGTH = 100;
 
 interface Props {
   defalutValues?: IRecipeForm;
@@ -53,8 +48,7 @@ export const RecipeForm = ({ defalutValues, submitTitle, onSubmit }: Props) => {
     },
   });
   const previewFoodImages = usePreviewImages(watch("pictures"));
-  const previewSteps = usePreviewSteps(watch("cooking_steps"));
-
+  
   const onError: SubmitErrorHandler<IRecipeForm> = (errors) => {
     const { ingredients, cooking_steps, ...errorFields } = errors;
 
@@ -96,16 +90,15 @@ export const RecipeForm = ({ defalutValues, submitTitle, onSubmit }: Props) => {
       <form className="flex-column" onSubmit={handleSubmit(onSubmit, onError)}>
         <SubjectBox title={submitTitle} className={styles.formContent}>
           <div className={styles.pictureSection}>
-            <label className={styles.pictureLabel}>
+            <label>
               사진
               <InfoTooltip
-                message="Cookidge는 500x500px 이미지를 제일 좋아합니다! 사진을 드래그하여 여러 개 선택하세요."
-                style={{ fontSize: "0.9em" }}
+                message="500x500px 이미지를 제일 좋아합니다! 사진을 드래그하여 여러 개 선택하세요."
               />
             </label>
             <InputFile
               id="pictures"
-              className={styles.pictureUpload}
+              className={styles.imageUploader}
               {...register(`pictures`, {
                 validate: () =>
                   !!previewFoodImages.length ||
@@ -113,9 +106,9 @@ export const RecipeForm = ({ defalutValues, submitTitle, onSubmit }: Props) => {
               })}
               multiple
             />
-            <ul className={styles.previewImageConatiner}>
+            <ul>
               {previewFoodImages?.map((image) => (
-                <li key={image} className={styles.previewImage}>
+                <li key={image} className={styles.imagePreview}>
                   <CldImg cldImg={image} />
                 </li>
               ))}
@@ -135,9 +128,11 @@ export const RecipeForm = ({ defalutValues, submitTitle, onSubmit }: Props) => {
             })}
           />
 
-          <InputBox
+          <TextArea
             id="introduce"
             label="소개"
+            length={watch('introduction')?.length || 0}
+            maxLength={INTRODUCE_LIMIT_LENGTH}
             placeholder="요리의 간단한 소개를 작성해주세요."
             {...register("introduction", {
               required: "요리의 간단한 소개를 작성해 주세요.",
@@ -148,7 +143,7 @@ export const RecipeForm = ({ defalutValues, submitTitle, onSubmit }: Props) => {
             })}
           />
 
-          <div className={styles.categoriesInput}>
+          <div className={styles.category}>
             <label htmlFor="category">카테고리</label>
             <select id="category" {...register("category", { required: true })}>
               {FOOD_CATEGORIES.map((category) => (
@@ -190,14 +185,14 @@ export const RecipeForm = ({ defalutValues, submitTitle, onSubmit }: Props) => {
 
           <IngredientField register={register} control={control} />
 
-          <CookingStepField
+          <StepField
             register={register}
             control={control}
-            previewImages={previewSteps}
+            defaultSteps={defalutValues?.cooking_steps as ICookingStep[] | undefined}
           />
         </SubjectBox>
 
-        <SubjectBox className={styles.submitContainer}>
+        <SubjectBox className={styles.submit}>
           <InputBox
             type="submit"
             value={submitTitle}
@@ -210,171 +205,4 @@ export const RecipeForm = ({ defalutValues, submitTitle, onSubmit }: Props) => {
   );
 };
 
-const IngredientField = ({
-  control,
-  register,
-}: Pick<UseFormReturn<IRecipeForm>, "register" | "control">) => {
-  const NAME_LIMIT_LENGTH = 20;
-  const QUANTITY_LIMIT_LENGTH = 8;
 
-  const {
-    fields: ingredientFields,
-    append: ingredientAppend,
-    remove: ingredientRemove,
-  } = useFieldArray({
-    name: "ingredients",
-    control,
-    rules: {
-      required: {
-        value: true,
-        message: "요리 재료를 1개 이상 입력해 주세요.",
-      },
-    },
-  });
-
-  return (
-    <FadeLayout key="ingredients">
-      <label>재료</label>
-      <ul className="w-full flex-column">
-        {ingredientFields.map((filed, index) => (
-          <FadeLayout key={filed.id}>
-            <li className="w-full flex-row">
-              <select
-                id="category"
-                {...register(`ingredients.${index}.category`, {
-                  required: true,
-                })}
-              >
-                {INGREDIENTS_CATEGORIES.map((category) => (
-                  <option key={category.text} value={category.text}>
-                    {category.emoji} {category.text}
-                  </option>
-                ))}
-              </select>
-              <InputBox
-                id="ingredient-1"
-                placeholder="재료 이름을 입력하세요."
-                {...register(`ingredients.${index}.name`, {
-                  required: `${index + 1}번째 재료 이름을 입력해 주세요.`,
-                  maxLength: {
-                    value: NAME_LIMIT_LENGTH,
-                    message: `재료 이름을 ${NAME_LIMIT_LENGTH}자 내외로 입력해 주세요.`,
-                  },
-                })}
-              />
-              <InputBox
-                id="quantity-1"
-                placeholder="양을 입력하세요. ex) 1개, 1큰술, 1컵"
-                {...register(`ingredients.${index}.quantity`, {
-                  required: `${index + 1}번째 재료 양을 입력해 주세요.`,
-                  maxLength: {
-                    value: QUANTITY_LIMIT_LENGTH,
-                    message: `재료 양을 ${QUANTITY_LIMIT_LENGTH}자 내외로 입력해 주세요.`,
-                  },
-                })}
-              />
-              <IconButton
-                Icon={CgRemoveR}
-                className={styles.removeButton}
-                onClick={(e) => {
-                  e.preventDefault();
-                  ingredientRemove(index);
-                }}
-              />
-            </li>
-          </FadeLayout>
-        ))}
-        <IconButton
-          Icon={RiAddLine}
-          className={styles.appendButton}
-          onClick={(e) => {
-            e.preventDefault();
-            ingredientAppend({ name: "", quantity: "", category: "고기" });
-          }}
-        >
-          추가
-        </IconButton>
-      </ul>
-    </FadeLayout>
-  );
-};
-
-const CookingStepField = ({
-  register,
-  control,
-  previewImages,
-}: Pick<UseFormReturn<IRecipeForm>, "register" | "control"> & {
-  previewImages: string[];
-}) => {
-  const INTRODUCE_LIMIT_LENGTH = 50;
-
-  const {
-    fields: cookingStepFields,
-    append: appendCookingStep,
-    remove: removeCookingStep,
-  } = useFieldArray({
-    name: "cooking_steps",
-    control,
-    rules: {
-      required: {
-        value: true,
-        message: "요리 과정을 1개 이상 입력해 주세요.",
-      },
-    },
-  });
-
-  return (
-    <div key="cookingSteps" className="flex-column">
-      <label>요리 과정</label>
-      <ul className="w-full flex-column">
-        {cookingStepFields.map((field, i) => (
-          <FadeLayout key={`${field.id}${i}`}>
-            <li className="w-full flex-row">
-              <h2>{i + 1}</h2>
-              <div className={styles.stepInputBox}>
-                <InputFile
-                  id={field.id}
-                  type="file"
-                  introduction="이미지 추가"
-                  previewUrl={previewImages[i]}
-                  {...register(`cooking_steps.${i}.picture`)}
-                />
-                <textarea
-                  id="step1"
-                  placeholder="조리 과정을 설명해주세요."
-                  className={styles.stepTextArea}
-                  {...register(`cooking_steps.${i}.instruction`, {
-                    required: `${i + 1}번째 요리 과정 설명을 입력해 주세요.`,
-                    maxLength: {
-                      value: INTRODUCE_LIMIT_LENGTH,
-                      message: `요리 과정 설명을 ${INTRODUCE_LIMIT_LENGTH}자 내외로 입력해 주세요.`,
-                    },
-                  })}
-                />
-              </div>
-              <IconButton
-                Icon={CgRemoveR}
-                onClick={(e) => {
-                  e.preventDefault();
-                  removeCookingStep(i);
-                }}
-                className={styles.removeButton}
-                style={{ alignItems: "stretch" }}
-              />
-            </li>
-          </FadeLayout>
-        ))}
-        <IconButton
-          Icon={RiAddLine}
-          className={styles.appendButton}
-          onClick={(e) => {
-            e.preventDefault();
-            appendCookingStep({ picture: "", instruction: "" });
-          }}
-        >
-          추가
-        </IconButton>
-      </ul>
-    </div>
-  );
-};
