3493785b50ee9fa143e4aa074bd6cebf013aaee8
unknown <rkawk1539@gmail.com>
2025-01-08 16:04:13 +0900

    모바일 크롬 요리 과정 미리보기 이미지 모두 비정상적으로 제거되는 현상 => 각 스탭을 독립적으로 createObjectURL을 하는 방법으로 수정 즉, step의 각 컴포넌트를 만듦

--- client/src/features/recipe/create/model/usePreviewSteps.ts
+++ client/src/features/recipe/create/model/usePreviewSteps.ts
@@ -1,31 +1,20 @@
 import { useEffect, useState } from "react";
 
-import type { ICookingStepInput } from "shared/api/recipe";
+export const usePreviewSteps = (defaultImage: string) => {
+  const [previewImage, setPreviewImage] = useState<string>(defaultImage);
 
-export const usePreviewSteps = (stepState: ICookingStepInput[]) => {
-  const [previewImage, setPreviewImage] = useState<string[]>([]);
-  // Effect 의존성 배열의 깊은 비교를 하기 위한 diffStep
-  // JSON.stringify를 직접 넣으면 경고 메시지 발생하기 때문에 변수로 할당
-  const diffStep =  JSON.stringify(stepState.map(({picture})=>typeof picture === "string" ? picture : picture?.[0]?.name))
-  
+  const previewOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if (!file) return setPreviewImage("");
+    setPreviewImage(URL.createObjectURL(file));
+  };
+
   useEffect(() => {
-    const imageFiles = stepState.map((step) => step.picture);
-    
-    const previewUrls = imageFiles.map((file) => {
-      if (!file?.length) return "";
-
-      return typeof file === "string" ? file : URL.createObjectURL(file[0]);
-    });
-
-    setPreviewImage(previewUrls);
-    
     return () => {
-      previewUrls.forEach((url) => {
-        if (!url) return;
-        return URL.revokeObjectURL(url);
-      });
+      URL.revokeObjectURL(previewImage);
     };
-  }, [stepState, diffStep]);
+  }, [previewImage]);
 
-  return previewImage;
+  return { previewImage, previewOnChange };
 };
