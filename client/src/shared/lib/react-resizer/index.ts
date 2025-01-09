5dfdcb1e74f52f3f4f5e65b62d1f1b39f7512e8f
unknown <rkawk1539@gmail.com>
2025-01-09 19:39:18 +0900

    react-reiszer 에러 테스트 14

--- client/src/shared/lib/react-resizer/index.ts
+++ client/src/shared/lib/react-resizer/index.ts
@@ -1,18 +1,22 @@
 import Resizer from "react-image-file-resizer";
 
 export const resizeFile = (file: File) => {
-  return new Promise((resolve) => {
-    Resizer.imageFileResizer(
-      file,
-      600,
-      600,
-      "JPEG",
-      100,
-      0,
-      (uri) => {
-        resolve(uri);
-      },
-      "base64"
-    );
+  return new Promise((resolve, reject) => {
+    try {
+      Resizer.imageFileResizer(
+        file,
+        600,
+        600,
+        "JPEG",
+        100,
+        0,
+        (uri) => {
+          resolve(uri);
+        },
+        "blob"
+      );
+    } catch (error) {
+      reject(error)
+    }
   });
 };
