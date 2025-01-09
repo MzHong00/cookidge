c01c636c5ce0d846ec7e178c822cd8b298b57a8c
unknown <rkawk1539@gmail.com>
2025-01-09 15:10:10 +0900

    레시피 create 에러 테스트6

--- client/src/features/recipe/create/ui/createRecipeForm/createRecipeForm.tsx
+++ client/src/features/recipe/create/ui/createRecipeForm/createRecipeForm.tsx
@@ -4,6 +4,13 @@ import type { IRecipeForm } from "shared/api/recipe";
 import { useConfirmDialogActions } from "shared/ui/confirmDialog";
 import { compressImage, compressImages } from "shared/lib/imageCompression";
 import { RecipeForm, useCreateRecipeMutation } from "../..";
+import imageCompression from "browser-image-compression";
+
+const options = {
+  maxSizeMB: 0.3,
+  initialQuality: 1,
+  useWebWorker: true,
+};
 
 export const CreateRecipeForm = () => {
   const { openDialogMessage } = useConfirmDialogActions();
@@ -13,21 +20,40 @@ export const CreateRecipeForm = () => {
     openDialogMessage({
       message: `${data.name} 레시피를 생성하시겠습니까?`,
       requestFn: async () => {
-        const compressedCookImages = (await compressImages(
-          data.pictures
-        )) as IRecipeForm["pictures"];
+        // const compressedCookImages = (await compressImages(
+        //   data.pictures
+        // )) as IRecipeForm["pictures"];
 
-        const stepImages = data.cooking_steps.map(
-          ({ instruction, picture }) => typeof picture === "string"
-          ? picture
-          : picture?.[0] || "",
+        const stepImages = data.cooking_steps.map(({ instruction, picture }) =>
+          typeof picture === "string" ? picture : picture?.[0] || ""
         );
 
-        const compressedStepImages = await compressImages(stepImages);
-        console.log("압축됨:",compressedCookImages);
-        console.log("압축됨:",compressedStepImages);
-        
+        // const compressedStepImages = await compressImages(stepImages);
+        // console.log("압축됨:", compressedCookImages);
+        // console.log("압축됨:", compressedStepImages);
 
+        try {
+        console.log(data.pictures);
+        const a = Array.from(data.pictures as (string|File)[]).map((file) => {
+            return typeof file === "string"
+              ? file
+              : imageCompression(file, options);
+          });
+          console.log(stepImages);
+  
+          const b = stepImages.map((file) => {
+            return typeof file === "string"
+              ? file
+              : imageCompression(file, options);
+          });
+  
+          const result = await Promise.all([...a, ...b]);
+        console.log(result);
+      } catch (error) {
+          console.log(error);
+          
+        }
+        
         // await mutateAsync({
         //   ...data,
         //   pictures: compressedCookImages,
--- client/src/features/recipe/update/ui/updateRecipeForm.tsx
+++ client/src/features/recipe/update/ui/updateRecipeForm.tsx
@@ -5,7 +5,13 @@ import { useConfirmDialogActions } from "shared/ui/confirmDialog";
 import { compressImage, compressImages } from "shared/lib/imageCompression";
 import { RecipeForm } from "features/recipe/create";
 import { useUpdateRecipeMutation } from "..";
+import imageCompression from "browser-image-compression";
 
+const options = {
+  maxSizeMB: 0.3,
+  initialQuality: 1,
+  useWebWorker: true,
+};
 interface Props {
   defalutValues: IRecipeForm;
 }
@@ -18,9 +24,9 @@ export const UpdateRecipeForm = ({ defalutValues }: Props) => {
     openDialogMessage({
       message: `레시피를 업데이트하시겠습니까?`,
       requestFn: async () => {
-        const compressedCookImages = (await compressImages(
-          data.pictures
-        )) as IRecipeForm["pictures"];
+        // const compressedCookImages = (await compressImages(
+        //   data.pictures
+        // )) as IRecipeForm["pictures"];
 
         const stepImages = data.cooking_steps.map(
           ({ instruction, picture }) => typeof picture === "string"
@@ -28,10 +34,34 @@ export const UpdateRecipeForm = ({ defalutValues }: Props) => {
           : picture?.[0] || "",
         );
 
-        const compressedStepImages = await compressImages(stepImages);
-        console.log("압축됨:",compressedCookImages);
-        console.log("압축됨:",compressedStepImages);
+        // const compressedStepImages = await compressImages(stepImages);
+        // console.log("압축됨:",compressedCookImages);
+        // console.log("압축됨:",compressedStepImages);
         
+
+        try {
+                console.log(data.pictures);
+                const a = Array.from(data.pictures as (string|File)[]).map((file) => {
+                    return typeof file === "string"
+                      ? file
+                      : imageCompression(file, options);
+                  });
+                  console.log(stepImages);
+          
+                  const b = stepImages.map((file) => {
+                    return typeof file === "string"
+                      ? file
+                      : imageCompression(file, options);
+                  });
+          
+                  const result = await Promise.all([...a, ...b]);
+                console.log(result);
+              } catch (error) {
+                  console.log(error);
+                  
+                }
+
+                
         // const compressedStepImages = await Promise.all(
         //   data.cooking_steps.map(async ({ instruction, picture }) => ({
         //     instruction: instruction,
