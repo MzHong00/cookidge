bf8148a2b64483e1f2203df75ea3ffda839b44cd
unknown <rkawk1539@gmail.com>
2025-01-10 17:43:40 +0900

    try catch 적용 및 confirm dialog의 process message 추가

--- client/src/features/recipe/create/ui/createRecipeForm/createRecipeForm.tsx
+++ client/src/features/recipe/create/ui/createRecipeForm/createRecipeForm.tsx
@@ -1,37 +1,51 @@
 import { SubmitHandler } from "react-hook-form";
 
+import { useAlertActions } from "shared/ui/alert";
 import type { IRecipeForm } from "shared/api/recipe";
 import { useConfirmDialogActions } from "shared/ui/confirmDialog";
-import { compressImage, compressImages } from "shared/lib/imageCompression";
+import { compressImage } from "shared/lib/imageCompression";
 import { RecipeForm, useCreateRecipeMutation } from "../..";
 
 export const CreateRecipeForm = () => {
-  const { openDialogMessage } = useConfirmDialogActions();
   const { mutateAsync } = useCreateRecipeMutation();
+  const { openDialogMessage, setProcessMessage } = useConfirmDialogActions();
+  const { alertEnqueue } = useAlertActions();
 
   const onSubmit: SubmitHandler<IRecipeForm> = async (data) => {
     openDialogMessage({
       message: `${data.name} 레시피를 생성하시겠습니까?`,
       requestFn: async () => {
-        const compressedCookImages = (await compressImages(
-          data.pictures
-        )) as IRecipeForm["pictures"];
+        try {
+          setProcessMessage("이미지 압축 중...");
+          const compressedCookImages = (await Promise.all(
+            Array.from(data.pictures as (string | File)[]).map((file) =>
+              typeof file === "string" ? file : compressImage(file)
+            )
+          )) as IRecipeForm["pictures"];
 
-        const compressedStepImages = await Promise.all(
-          data.cooking_steps.map(async ({ instruction, picture }) => ({
-            instruction: instruction,
-            picture:
-              typeof picture === "string"
-                ? picture
-                : await compressImage(picture?.[0]),
-          }))
-        );
-
-        await mutateAsync({
-          ...data,
-          pictures: compressedCookImages,
-          cooking_steps: compressedStepImages,
-        });
+          const compressedStepImages = await Promise.all(
+            data.cooking_steps.map(async ({ instruction, picture }) => ({
+              instruction: instruction,
+              picture:
+                typeof picture === "string"
+                  ? picture
+                  : picture && ((await compressImage(picture?.[0])) as File),
+            }))
+          );
+          
+          setProcessMessage("서버에 전송 중...");
+          await mutateAsync({
+            ...data,
+            pictures: compressedCookImages,
+            cooking_steps: compressedStepImages,
+          });
+        } catch (error) {
+          alertEnqueue({
+            message:
+              "레시피 생성에 실패하였습니다. ※ 모바일 환경에서 원활하지 않을 수 있습니다.",
+            type: "error",
+          });
+        }
       },
     });
   };
--- client/src/features/recipe/update/ui/updateRecipeForm.tsx
+++ client/src/features/recipe/update/ui/updateRecipeForm.tsx
@@ -1,8 +1,9 @@
 import { SubmitHandler } from "react-hook-form";
 
+import { useAlertActions } from "shared/ui/alert";
 import type { IRecipeForm } from "shared/api/recipe/type";
 import { useConfirmDialogActions } from "shared/ui/confirmDialog";
-import { compressImage, compressImages } from "shared/lib/imageCompression";
+import { compressImage } from "shared/lib/imageCompression";
 import { RecipeForm } from "features/recipe/create";
 import { useUpdateRecipeMutation } from "..";
 
@@ -11,32 +12,45 @@ interface Props {
 }
 
 export const UpdateRecipeForm = ({ defalutValues }: Props) => {
-  const { openDialogMessage } = useConfirmDialogActions();
+  const { openDialogMessage, setProcessMessage } = useConfirmDialogActions();
   const { mutateAsync } = useUpdateRecipeMutation(defalutValues._id);
+  const { alertEnqueue } = useAlertActions();
 
   const onSubmit: SubmitHandler<IRecipeForm> = async (data) => {
     openDialogMessage({
       message: `레시피를 업데이트하시겠습니까?`,
       requestFn: async () => {
-        const compressedCookImages = (await compressImages(
-          data.pictures
-        )) as IRecipeForm["pictures"];
+        try {
+          setProcessMessage("이미지 압축 중...");
+          const compressedCookImages = (await Promise.all(
+            Array.from(data.pictures as (string | File)[]).map((file) =>
+              typeof file === "string" ? file : compressImage(file)
+            )
+          )) as IRecipeForm["pictures"];
 
-        const compressedStepImages = await Promise.all(
-          data.cooking_steps.map(async ({ instruction, picture }) => ({
-            instruction: instruction,
-            picture:
-              typeof picture === "string"
-                ? picture
-                : await compressImage(picture?.[0]),
-          }))
-        );
+          const compressedStepImages = await Promise.all(
+            data.cooking_steps.map(async ({ instruction, picture }) => ({
+              instruction: instruction,
+              picture:
+                typeof picture === "string"
+                  ? picture
+                  : await compressImage(picture?.[0]),
+            }))
+          );
 
-        await mutateAsync({
-          ...data,
-          pictures: compressedCookImages,
-          cooking_steps: compressedStepImages,
-        });
+          setProcessMessage("서버에 전송 중...");
+          await mutateAsync({
+            ...data,
+            pictures: compressedCookImages,
+            cooking_steps: compressedStepImages,
+          });
+        } catch (error) {
+          alertEnqueue({
+            message:
+              "레시피 업데이트에 실패하였습니다. ※ 모바일 환경에서 원활하지 않을 수 있습니다.",
+            type: "error",
+          });
+        }
       },
     });
   };
