75a435d940cdecb21b8377adfc7bb6847b0aebbc
unknown <rkawk1539@gmail.com>
2025-01-15 14:01:19 +0900

    폴더 이름 변경 및 미사용 콘솔 제거

---
+++ client/src/widgets/myIngredients/index.ts
@@ -0,0 +1,4 @@
+export { MyIngredientWidget } from "./ui/myIngredientWidget";
+
+export { useFilterIngredients } from "./model/useFilterIngredients";
+export { useSortIngredients } from "./model/useSortIngredients";
---
+++ client/src/widgets/myIngredients/model/useSortIngredients.ts
@@ -0,0 +1,28 @@
+import { useMemo, useState } from "react";
+
+import type { IIngredient } from "shared/api/ingredient";
+import { INGREDIENT_SORT_TYPES } from "entities/ingredient";
+
+export const useSortIngredients = (ingredients: IIngredient[]) => {
+  const [sortTypeValue, setSortTypeValue] = useState<
+    (typeof INGREDIENT_SORT_TYPES)[number]
+  >(INGREDIENT_SORT_TYPES[0]);
+
+  const sortResult = useMemo(() => {
+    if (sortTypeValue === INGREDIENT_SORT_TYPES[0]) {
+      return ingredients.sort((a, b) => b.category.localeCompare(a.category));
+    }
+
+    return ingredients.sort((a, b) => b.expired_at.localeCompare(a.expired_at));
+  }, [ingredients, sortTypeValue]);
+
+  const onChangesortTypeValue = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSortTypeValue(e.target.value as (typeof INGREDIENT_SORT_TYPES)[number]);
+  };
+
+  return {
+    sortTypeValue,
+    sortResult,
+    onChangesortTypeValue,
+  };
+};
