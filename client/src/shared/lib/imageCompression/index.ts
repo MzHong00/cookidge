9390f381828dc78c8ba3186f8372be3c1d413ef9
unknown <rkawk1539@gmail.com>
2025-01-19 18:35:29 +0900

    이미지 압축 imageCompression함수를 사용해 base64로도 반환할 수 있는 함수 생성

--- client/src/shared/lib/imageCompression/index.ts
+++ client/src/shared/lib/imageCompression/index.ts
@@ -1,12 +1,31 @@
-import imageCompression from "browser-image-compression";
+import imageCompression, { Options } from "browser-image-compression";
 
-const options = {
+const defaultOptions = {
   maxSizeMB: 0.3,
   initialQuality: 1,
-  useWebWorker: true
+  useWebWorker: true,
 };
 
-export const compressImage = (file?: File) => {
+export const compressImage = (file?: File, options?: Options) => {
   if (!file) return;
-  return imageCompression(file, options);
+  return imageCompression(file, { ...defaultOptions, ...options });
 };
+
+export const compressImageToBase64 = async (file?: File, options?: Options) => {
+  if (!file) return;
+
+  const compressedImage= await compressImage(file, options);
+  const compressedBase64Image = await blobToBase64(compressedImage as Blob);
+
+  return compressedBase64Image;
+};
+
+export function blobToBase64(blob: Blob): Promise<string | ArrayBuffer | null> {
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(blob); // base64 형태의 data url로 데이터를 읽어 변환
+
+    // 데이터를 모두 읽으면 onload 이벤트를 발생
+    reader.onload = () => resolve(reader.result);
+  });
+}
