c4adfa03f11094da71d45513d670db71c6d10775
unknown <rkawk1539@gmail.com>
2025-01-27 16:02:56 +0900

    테스트 계정 로그인시 window reload

--- client/src/features/user/login/model/useTestFormSubmit.ts
+++ client/src/features/user/login/model/useTestFormSubmit.ts
@@ -1,20 +1,16 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { SubmitHandler } from "react-hook-form";
 
 import { AuthService } from "shared/api/auth";
 
 export const useTestFormSubmit = () => {
-  const navigate = useNavigate();
   const [errMsg, setErrMsg] = useState<string>("");
 
   const onSubmit: SubmitHandler<{ code: string }> = async (data) => {
     const errMsg = await AuthService.testAccountLogin(data.code);
-    if (errMsg) {
-      return setErrMsg(errMsg);
-    }
+    if (errMsg) return setErrMsg(errMsg);
 
-    navigate("/dashboard");
+    window.location.reload();
   };
 
   return {
