e22c1f482d9f88a2ebea7215738b4f46c75a9b87
unknown <rkawk1539@gmail.com>
2025-02-01 14:26:47 +0900

    comment List 분리 후 widget으로 생성

--- client/src/widgets/comment/ui/commentWidget.tsx
+++ client/src/widgets/comment/ui/commentWidget.tsx
@@ -1,17 +1,30 @@
+import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
+
 import type { IRecipe } from "shared/api/recipe";
 import { SubjectBox } from "shared/ui/subjectBox";
-import { CommentList } from "entities/comment";
+import { useIntersectionObserver } from "shared/hooks/useIntersectionObserver";
+import { Comment, CommentQueries } from "entities/comment";
 import { CreateComment } from "features/comment/create";
 
-interface Props {
+export const CommentWidget = ({ recipe_id }: {
   recipe_id: IRecipe["_id"];
-}
-
-export const CommentWidget = ({ recipe_id }: Props) => {
+}) => {
+  const { data, fetchNextPage, hasNextPage } = useSuspenseInfiniteQuery(
+    CommentQueries.infiniteQuery({ recipeId: recipe_id, limit: 10 })
+  );
+  const { setTarget } = useIntersectionObserver({ hasNextPage, fetchNextPage });
+  
   return (
     <SubjectBox title="댓글" style={{ border: "none" }}>
       <CreateComment recipeId={recipe_id} />
-      <CommentList recipe_id={recipe_id} />
+      <div className="flex-column">
+        {data?.pages.map((page) =>
+          page.map((comment) => (
+            <Comment key={comment._id} commentData={comment} />
+          ))
+        )}
+        <div id="observer" ref={setTarget} style={{ height: "10%" }} />
+      </div>
     </SubjectBox>
   );
 };
