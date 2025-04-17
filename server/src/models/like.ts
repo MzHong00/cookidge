5a8ed1120b2927d816a27b1e1d1421faca4454cc
unknown <rkawk1539@gmail.com>
2025-04-17 17:32:19 +0900

    fix: like 중복 방지용 복합키 설정

--- server/src/models/like.ts
+++ server/src/models/like.ts
@@ -3,8 +3,9 @@ import mongoose, { Schema } from "mongoose";
 import { ILike } from "../interface/ILike";
 
 const LikeSchema = new Schema<ILike>({
-  user_id: { type: mongoose.Types.ObjectId,required: true },
+  user_id: { type: mongoose.Types.ObjectId, required: true },
   recipe_id: { type: mongoose.Types.ObjectId, required: true },
 });
 
+LikeSchema.index({ user_id: 1, recipe_id: 1 }, { unique: true });
 export const Like = mongoose.model<ILike>("Like", LikeSchema);
